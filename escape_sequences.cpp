64f9833c22b141ab1f2b275e3bad448533a635cf
Ruslan Marchenko <151775510+Goody-Morn@users.noreply.github.com>
2024-12-08 22:00:44 +0100

    Create escape_sequences.cpp

---
+++ escape_sequences.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "\tEscape Sequences\n\n";
+	cout << "\\a\tBell (alert)\n";
+	cout << "\\b\tBackspace\n";
+	cout << "\\n\tNew line\n";
+	cout << "\\r\tCarriage return\n";
+	cout << "\\t\tHorizontal tab\n";
+	cout << "\\\\\tBackslash \\\n";
+	cout << "\\t\tHorizontal tab\n";
+	cout << "\\\"\tDouble quotation mark \"\n";
+	cout << "\\\'\tSingle quotation mark '\n";
+	return 0;
+}
